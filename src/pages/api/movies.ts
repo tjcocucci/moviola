ac4800914aecd7276d51ba9fdd5bc465db8d4694
tadeojcocucci <tadeojcocucci@gmail.com>
2023-06-10 21:02:43 -0300

    minor

--- src/pages/api/movies.ts
+++ src/pages/api/movies.ts
@@ -4,13 +4,20 @@ const API_URL = `https://api.themoviedb.org/3/`;
 const API_KEY = 'ba282fc7777a85594b4d09bffedbb258';
 const IMAGE_PATH = 'https://image.tmdb.org/t/p/w500';
 
+const DEFAULT_PARAMS = {
+  language: 'en-US',
+  sort_by: 'popularity.desc',
+  include_adult: 'false',
+  include_video: 'false',
+}
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
     try {
       const params = new URLSearchParams(req.query);
-      const response = await fetch(
-        `${API_URL}discover/movie?api_key=${API_KEY}&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&${params}`
-      );
+      const defaultParams = new URLSearchParams(DEFAULT_PARAMS);
+      const url = `${API_URL}discover/movie?api_key=${API_KEY}&${defaultParams}&${params}`;
+      const response = await fetch(url);
       const data = await response.json();
       const results = data.results;
 
