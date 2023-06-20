4d51ad555583b99d079686cf9dffef841d6468c7
tadeojcocucci <tadeojcocucci@gmail.com>
2023-06-20 12:50:01 -0300

    unnecessary import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Sidebar from '../components/Sidebar';
-import clsx from 'clsx';
 import { MainCard } from '@/components/MainCard';
 
 const inter = Inter({ subsets: ['latin'] });
